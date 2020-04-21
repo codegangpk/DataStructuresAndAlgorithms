6f317dea5afb6bac472e39e8e4c1d644bfed74cc
Paul Kim <parahop94@gmail.com>
2020-04-21 10:46:36 +0900

    add stack challenges

--- DataStructuresAndAlgorithms/ViewControllers/TestViewController.swift
+++ DataStructuresAndAlgorithms/ViewControllers/TestViewController.swift
@@ -13,14 +13,12 @@ class TestViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        test1()
-        test2()
-        test3()
+        stackChallenge2Solution()
     }
 }
 
 extension TestViewController {
-    private func test1() {
+    private func stackTest1() {
         var stack = Stack<Int>()
         stack.push(1)
         stack.push(2)
@@ -35,17 +33,75 @@ extension TestViewController {
         }
     }
     
-    private func test2() {
+    private func stackTest2() {
         var stack = Stack(elements: ["A", "B", "C", "D"])
         print(stack)
         stack.pop()
         print(stack)
     }
     
-    private func test3() {
+    private func stackTest3() {
         var stack: Stack = [1.0, 2.0, 3.0, 4.0]
         print(stack)
         stack.pop()
         print(stack)
+    }
+    
+    private func stackChallenge1() {
+        let array = [1, 2, 3, 4, 5]
+        
+        func printInReverse<T>(_ array: [T]) {
+            var stack = Stack(elements: array)
+            
+            while let element = stack.pop() {
+                print(element)
+            }
+        }
+        
+        printInReverse(array)
+    }
+    
+    private func stackChallenge2Try() {
+        var testString1 = "h((e))llo(world)()"
+        
+        func checkBalance(text: String) -> Bool {
+            let textArray = testString1.compactMap { String($0) }
+            var stack: Stack = Stack(elements: textArray)
+            
+            var paranthesisCount: Int = 0
+            
+            while let letter = stack.pop() {
+                if letter == "(" || letter == ")" {
+                    paranthesisCount += 1
+                }
+            }
+            
+            return paranthesisCount > 0 && paranthesisCount % 2 == 0
+        }
+        
+        print(checkBalance(text: testString1))
+        //this solution is only interested in the count. not the asthetic balance.
+    }
+    
+    private func stackChallenge2Solution() {
+        var testString1 = "h((e))llo(world)()"
+        
+        func checkBalance(text: String) -> Bool {
+            var stack = Stack<Character>()
+            
+            for character in testString1 {
+                if character == "(" {
+                    stack.push(character)
+                } else if character == ")" {
+                    if stack.isEmpty {
+                        return false
+                    } else {
+                        stack.pop()
+                    }
+                }
+            }
+            return stack.isEmpty
+        }
+        print(checkBalance(text: testString1))
     }
 }
